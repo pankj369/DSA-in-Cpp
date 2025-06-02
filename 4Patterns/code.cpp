3231a18b528ecdaddb2d33bbc5ad68cbd76c4c45
Pankaj Singh <psingh227003@gmail.com>
2025-06-02 09:02:00 +0530

    hollow diamond pattern

--- 4Patterns/code.cpp
+++ 4Patterns/code.cpp
@@ -122,21 +122,85 @@ int main()
   //   cout << endl;
   // }
   // ?print inverted triangle pattern
+  // int n = 4;
+  // int num=1;
+  // for (int i = 0; i < n; i++)
+  // {
+  //   // spaces
+  //   for (int j = 0; j < i; j++)
+  //   {
+  //     cout << " ";
+  //   }
+  //   // nums
+  //   for (int j = 0; j < n - i; j++)
+  //   {
+  //     cout << (i + 1); //<< " "
+  //   }
+
+  //   cout << endl;
+  // }
+
+  // ?pyramid pattern
+  // int n = 4;
+  // int num = 1;
+  // for (int i = 0; i < n; i++)
+  // {
+  //   // spaces
+  //   for (int j = 0; j < n - i - 1; j++)
+  //   {
+  //     cout << " ";
+  //   }
+  //   // num1=i+1;
+  //   for (int j = 1; j <= i + 1; j++)
+  //   {
+  //     cout << j;
+  //   }
+  //   // num2;
+  //   for (int j = i; j > 0; j--)
+  //   {
+  //     cout << j;
+  //   }
+  //   cout << endl;
+  // }
+
+  // ?Hollow diamond pattern
   int n = 4;
-  int num=1;
-  for (int i = 0; i < n; i++)
-  {
-    // spaces
-    for (int j = 0; j < i; j++)
+  for (int i = 0; i < n; i++)           // outer loop
+  {                                     // spaces
+    for (int j = 0; j < n - i - 1; j++) // inner loop for upper area
     {
       cout << " ";
     }
-    // nums
-    for (int j = 0; j < n - i; j++)
+    cout << "*";
+    // inner spaces
+    if (i != 0)
     {
-      cout << (i + 1); //<< " "
+      for (int j = 0; j < 2 * i - 1; j++)
+      {
+        cout << " ";
+      }
+      cout << "*";
+    }
+    cout << endl;
+  }
+  // buttom area
+  for (int i = n - 2; i >= 0; i--)
+  {
+    // spaces
+    for (int j = 0; j < n - i - 1; j++)
+    {
+      cout << " ";
     }
 
+    cout << "*";
+    if (i != 0)
+    { // inner spaces
+      for (int j = 0; j < 2 * i - 1; j++)
+      {
+        cout << " ";
+      }
+      cout << "*";
+    }
     cout << endl;
   }
   return 0;
