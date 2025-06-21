65660efbb7de9b97da789e5b5d106ff0e2c6b23f
Pankaj Singh <psingh227003@gmail.com>
2025-06-21 11:47:36 +0530

    practice question

--- 4Patterns/practicesame.cpp
+++ 4Patterns/practicesame.cpp
@@ -93,7 +93,7 @@ int main()
   //   }
   //   cout<<endl;
   // }
-int n=4;
+  // int n=5;
   // for (int i = 0; i < n; i++)
   // {
   //   for (int j = i + 1; j > 0; j--)
@@ -111,11 +111,125 @@ int n=4;
   //   cout<<endl;
   // }
 
-  for(int i=0; i<n; i++){
-    for(int j=0;j<i+1; j++){
-      cout<<j;
+  // for(int i=0; i<n; i++){
+  //   for(int j=1;j<=i+1; j++){
+  //     cout<<j;
+  //   }
+  //   cout<<endl;
+  // }
+
+  //  for(int i=0; i<n; i++){
+  //   for(int j=i+1; j>0; j--){
+  //     cout<<j;
+  //  }
+  //  cout<<endl;
+  // }
+
+  // for(int i=0; i<n; i++){
+  //   char ch='A'+i;
+  //   for(int j=i+1; j>0; j--){
+  //     cout<<ch;
+  //     ch--;
+  //   }
+  //   cout<<endl;
+  // }
+  //  int num=1;
+  // for(int i=0; i<n; i++){
+  //   for(int j=0; j<i; j++){
+  //     cout<<num;
+  //     num++;
+  //   }
+  //   cout<<endl;
+  // }
+  //! using backward
+  // int num=1;
+  // for(int i=0; i<n; i++){
+  //   for(int j=i+1; j>0; j--){
+  //     cout<<num;
+  //     num++;
+  //   }
+  //   cout<<endl;
+  // }
+
+  //   char ch='A';
+  // for(int i=0; i<n; i++){
+  //   for(int j=0; j<i; j++){
+  //     cout<<ch;
+  //     ch++;
+  // }
+  // cout<<endl;
+  // }
+
+  // int n=4;
+  // for(int i=0; i<n; i++){
+  //   for(int j=0; j<i; j++){
+  //     cout<<" ";
+  //   }
+  //   for(int j=0; j<n-i; j++){
+  //     cout<<(i+1);
+  //   }
+  //   cout<<endl;
+  // }
+
+  // int n = 5;
+  // for (int i = 0; i < n; i++)
+  // {
+  //   for (int j = 0; j < n - i - 1; j++)
+  //   {
+  //     cout << " "; // spaces 1 step
+  //   }
+  //   for (int j = 1; j <= i; j++)
+  //   {
+  //     cout << j; // print increasing number form 1 to i
+  //   }
+  //   for (int j = i-1; j > 0; j--)
+  //   {
+  //     cout << j; // print print decreasing number from i to 1
+  //   }
+  //   cout << endl;
+  // }
+
+  int n = 4;
+  for (int i = 0; i < n; i++) // upper half
+  {
+    for (int j = 0; j < n - i - 1; j++)
+    {
+      cout << " "; // spaces
     }
-    cout<<endl;
+    cout << "*"; // first star
+
+    // hollow space between stars
+    if (i != 0)
+    {
+      for (int j = 0; j < 2 * i - 1; j++)
+      {
+        cout << " ";
+      }
+      cout << "*";
+    }
+    cout << endl;
+  }
+  // lower half
+  for (int i = n - 2; i >= 0; i--)
+  {
+    // spaces
+    for (int j = 0; j < n - i - 1; j++)
+    {
+      cout << " ";
+    }
+    cout << "*";
+
+    // hollow space between stars
+
+    if (i != 0)
+    {
+      for (int j = 0; j < 2 * i - 1; j++)
+      {
+        cout << " ";
+      }
+      cout << "*";
+    }
+    cout << endl;
   }
   return 0;
 }
