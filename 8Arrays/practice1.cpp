25fbdbba017543f86048a4af6dd5980153b65a11
Pankaj Singh <psingh227003@gmail.com>
2025-06-27 22:01:45 +0530

    practice

---
+++ 8Arrays/practice1.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+// TODO:
+// ! 1. WAF to calculate sum and product of all numbers in an array
+// int main()
+// {
+//   int arr[5] = {1, 2, 3, 4, 5};
+//   int size = sizeof(arr) / sizeof(arr[0]);
+//   int sum = 0;
+//   int product = 1;
+//   for (int i = 0; i < size; i++)
+//   {
+//     sum += arr[i];
+//     product *= arr[i];
+//   }
+//     cout << "sum = " << sum << endl;
+//     cout << "product = " << product << endl;
+
+//   return 0;
+// }
+
+// ! reverse an array
+// void reverseArr(int arr[], int size)
+// {
+//   int start = 0;
+//   int end = size - 1;
+//   while (start < end)
+//   {
+//     swap(arr[start], arr[end]);
+//     start++;
+//     end--;
+//   }
+// }
+// int main()
+// {
+//   int arr[5] = {10, 20, 30, 40, 50};
+//   int size = 5;
+//   reverseArr(arr, size);
+//   for (int i = 0; i < size; i++)
+//   {
+//     cout << arr[i] << " " << endl;
+//   }
+//   return 0;
+// }
+// ! 2. WAF to swap the max and min number of an array.
+int main()[
+  int arr[5]={2,32,4343,545,6};
+  int size=5;
+  
+]
+// ! 3. WAF to print all the unique values in an array.
+// ! 4. WAF to print intersection of 2 arrays.
