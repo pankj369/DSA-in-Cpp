e6153e29a8d4c9ccc1f8f8bad46640d6a1ea167b
Pankaj Singh <psingh227003@gmail.com>
2025-07-19 20:23:36 +0530

    practice patterns

---
+++ 4Patterns/codebabber-patterns-problem.cpp
@@ -0,0 +1,104 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+/*  
+int i=1, n=4;
+while(i<=n){
+  int j=1;
+  while(j<=n-i+1){
+    cout<<"*";`
+    j++;
+  }
+  cout<<endl;
+  i++;
+}
+
+int i=1, n=5;
+while(i<=n){
+  int space=1;
+  while(space<i){
+    cout<<" ";
+    space++;
+  }
+  int j=1;
+  while(j<=n-i+1){
+    cout<<"*";
+    j++;
+  }
+  cout<<endl;
+  i++;
+}
+
+
+int i=1, n=5;
+while(i<=n){
+  int space=1;
+
+  while(space<i){
+    cout<<" ";
+    space++;
+  }
+  int j=1;
+  while(j<=n-i+1){
+    cout<<i;
+    j++;
+  }
+  cout<<endl;
+  i++;
+}
+ 
+  int i=1, n=4;
+  while(i<=n){
+    int space=1;
+    while(space<n-i){
+      cout<<" ";
+      space++;
+    }
+    int j=1;
+    while(j<=i){
+      cout<<i;
+      j++;
+    }
+    cout<<endl;
+    i++;
+  }
+
+
+int i=0;
+int n=4;
+while(i<=n){
+  int space =0;
+  while(space<i){
+    cout<<" ";
+    space++;
+  }
+  int j=i+1;
+  while(j<=n){
+    cout<<j;
+    j++;
+  }
+  cout<<endl;
+  i++;
+}
+
+   int i=1;
+   int num=1;
+   int n=4;
+   while(i<=n){
+    int space=n-1;
+    cout<<" ";
+    space++;
+    int j=1;
+    while(j<=i){
+      cout<<num<<" ";
+      num++;
+      j++;
+    }
+    cout<<endl;
+    i++;
+   }
+     */
+
+return 0;
+}
