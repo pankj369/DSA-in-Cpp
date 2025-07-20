ecff98c37ffdbebffd3d56e54cddd1819bd3e778
Pankaj Singh <psingh227003@gmail.com>
2025-07-20 23:21:26 +0530

    practice

---
+++ 3conditional&loops/question-practice.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+using namespace std;
+//! print numbers form 1 to 10 using loop
+/*int main(){
+int n=10;
+for(int i=1; i<=n; i++){
+  cout<<i<<endl;
+}
+return 0;
+}
+*/
+//! print even numbers from 1 to 50;
+/*int main(){
+  int n=50;
+    for(int i=1; i<=n; i++){
+    if(i%2==0){
+      cout<<i<<" ";
+    }
+  }
+  return 0;
+}
+*/
+/*
+int main(){
+  int n=10;
+  int num=7;
+  for(int i=1; i<=n; i++){
+    cout<< num*i<<endl;
+  }
+  return 0;
+}
+*/
+/*
+int main(){
+  int n=10;
+  for(int i=1; i<=n; i++){
+     cout<<i*i<<" ";
+   }
+  return 0;
+}
+*/
