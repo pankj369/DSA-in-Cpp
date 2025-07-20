a54d7c2a0a07bd382fd5e82ee7e9c0d53638b3ee
Pankaj Singh <psingh227003@gmail.com>
2025-07-20 08:47:12 +0530

    pattern problems

--- 4Patterns/codebabber-patterns-problem.cpp
+++ 4Patterns/codebabber-patterns-problem.cpp
@@ -82,13 +82,16 @@ while(i<=n){
   i++;
 }
 
-   int i=1;
+int i=1;
    int num=1;
-   int n=4;
+   int n=3;
    while(i<=n){
-    int space=n-1;
-    cout<<" ";
+    int space=1;
+    while(space<=n-i){
+      cout<< "  ";//DOUBLE SPACE FOR ALIGNMENT
     space++;
+    }
+    
     int j=1;
     while(j<=i){
       cout<<num<<" ";
@@ -98,7 +101,50 @@ while(i<=n){
     cout<<endl;
     i++;
    }
-     */
 
+
+  int n=5;
+  int i=1;
+  while(i<=n){
+    int num1=1;
+    while(num1<=n-i+1){
+      cout<<num1<<" ";
+      num1++;
+    }
+    int star=1;
+    while(star <=2 * (i-1)){
+      cout<< "* ";
+      star++;
+    }
+    int num2=n-i+1;
+    while(num2>=1){
+      cout<<num2<< " ";
+      num2--;
+    }
+    cout<<endl;
+    i++;
+  }
+    */
+  int n=4;
+  int i=1;
+  while(i<=n){
+    int space=1;
+    while(space<=n-i+1){
+      cout<<" ";
+      space++;
+    }
+    int j=1;
+    while(j<=i){
+      cout<<j;
+      j++;
+    }
+    int k=i-1;
+    while(k>0){
+      cout<<k;
+      k--;
+    }
+    cout<<endl;
+    i++;
+  }
 return 0;
 }
--- 4Patterns/practicesame.cpp
+++ 4Patterns/practicesame.cpp
@@ -171,24 +171,24 @@ int main()
   //   cout<<endl;
   // }
 
-  // int n = 5;
-  // for (int i = 0; i < n; i++)
-  // {
-  //   for (int j = 0; j < n - i - 1; j++)
-  //   {
-  //     cout << " "; // spaces 1 step
-  //   }
-  //   for (int j = 1; j <= i; j++)
-  //   {
-  //     cout << j; // print increasing number form 1 to i
-  //   }
-  //   for (int j = i-1; j > 0; j--)
-  //   {
-  //     cout << j; // print print decreasing number from i to 1
-  //   }
-  //   cout << endl;
-  // }
-
+  int n = 5;
+  for (int i = 0; i < n; i++)
+  {
+    for (int j = 0; j < n - i - 1; j++)
+    {
+      cout << " "; // spaces 1 step
+    }
+    for (int j = 1; j <= i; j++)
+    {
+      cout << j; // print increasing number form 1 to i
+    }
+    for (int j = i-1; j > 0; j--)
+    {
+      cout << j; // print print decreasing number from i to 1
+    }
+    cout << endl;
+  }
+/*
   int n = 4;
   for (int i = 0; i < n; i++) // upper half
   {
@@ -231,5 +231,6 @@ int main()
     }
     cout << endl;
   }
+    */
   return 0;
 }
