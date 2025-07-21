d7a25bb83b9f907285ae77f514634fd5065a98c2
Pankaj Singh <psingh227003@gmail.com>
2025-07-21 22:33:22 +0530

    patterns questions practice

--- 3conditional&loops/question-practice.cpp
+++ 3conditional&loops/question-practice.cpp
@@ -39,3 +39,55 @@ int main(){
   return 0;
 }
 */
+/*
+int main(){
+int n = 5;
+
+for(int i=1; i<=n; i++){//rows
+  for (int j=5; j>=n-i+1; j--){//columns
+    cout<<j;//print stars
+  }
+  cout<<endl;
+}
+return 0;
+}
+*/
+/*
+int main(){
+  // int n=9;
+  for(int i=9; i>5; i--){
+    cout<<"+"<<endl;
+    // cout<<endl;
+  }
+  // cout<<endl;
+  return 0;
+}
+*/
+
+ int main(){
+
+  for(int rows4=1; rows4<=2*5+1; rows4++){
+    int totalCol= 45;
+    for(int columns4=0; columns4<=to ;columns4++){
+      cout<<"*";
+    }
+    cout<<endl;
+  }
+
+
+  for(int rows3=1; rows3<=5; rows3++){
+    for(int columns3=1; columns3<=5-rows3+1;columns3++){
+      cout<<columns3;
+    }
+    cout<<endl;
+  }
+ 
+  for(int rows=1; rows<=5; rows++){
+    //for every row, run the col
+  for(int columns=1; columns<=rows;columns++){
+    cout<<columns;
+  }
+  cout<<endl;
+ }
+ return 0;
+ }
