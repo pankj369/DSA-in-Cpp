82571df149878c5715cd17474527ff6a32ee6051
Pankaj Singh <psingh227003@gmail.com>
2025-07-24 21:54:55 +0530

    patterns problem form yt

--- 3conditional&loops/question-practice.cpp
+++ 3conditional&loops/question-practice.cpp
@@ -64,30 +64,185 @@ int main(){
 }
 */
 
- int main(){
-
-  for(int rows4=1; rows4<=2*5+1; rows4++){
-    int totalCol= 45;
-    for(int columns4=0; columns4<=to ;columns4++){
+int main()
+{
+  int n=5;
+  for(int i=1; i<=n; i++){
+    for(int spaces=1; spaces<=n-i; spaces++){
+      cout<<" ";
+    }
+    for(int j=1; j<=i; j++){
+      cout<<j<<" ";
+    }
+    cout<<endl;
+  }
+  /*
+  int n=5;
+  //top half
+  for(int i=1; i<=n; i++){
+    for(int spaces=1; spaces<=n-i; spaces++){
+      cout<<" ";
+    }
+    for(int j=1; j<=2*i-1; j++){
+      if(j==1 || j==2*i-1){
+        cout<<"*";
+      }else{
+        cout<<" ";
+      }
+    }
+    cout<<endl;
+  }
+  //bottom half
+  for(int i=n-1; i>=1; i--){
+    for(int spaces=1; spaces<=n-i; spaces++){
+      cout<<" ";
+    }
+    for(int j=1; j<=2*i-1; j++){
+      if(j==1 || j==2*i-1){
+        cout<<"*";
+      }else{
+        cout<<" ";
+      }
+    }
+    cout<<endl;
+  }
+  */
+  /*
+  int n=5;
+  for(int i =n; i>=1; i--){ //outer loop -controls the no. of rows
+    for(int spaces=1; spaces<=n-i; spaces++){
+      cout<<" ";//print leading spacces to shift the triangle right 
+    }
+    for(int j=1; j<=n; j++){ //inner loop controls wht to print in each row
+      if(j==1|| j==i ||i==n){// print star at start, i-th position and full row if i==n
+        cout<<"* ";
+      }else{ 
+        cout<<"  ";//print space btwn stars here,  print two spaces for alignment
+      }
+    }
+    cout<<endl;
+  }
+  */
+    /*
+  int n=5;
+  for(int i=1; i<=n; i++){
+    for(int spaces=1; spaces<=n-i; spaces++){
+      cout<<" ";
+    }
+    //print stars and internal spaces
+    for(int j=1; j<=i; j++){
+      if(j==1 || j==i || i==n){
+        cout<<"* ";
+      }else{
+        cout<<"  ";
+      }
+    }
+    cout<<endl;
+  }
+  */
+  /*
+  int n=5; 
+  for(int i=1; i<=n; i++){
+    for(int spaces=1; spaces<=i; spaces++ ){
+      cout<<" ";
+    }
+    for(int j=1; j<=n-i+1; j++){
+      cout<<"* ";
+    }
+    cout<<endl;
+  }
+  for(int i=1; i<=n; i++){
+         for(int spaces2=1; spaces2<=n-i+1; spaces2++){
+      cout<<" ";
+    }
+    for(int k=1; k<=i;k++){
+      cout<<"* ";
+    }
+    cout<<endl;
+  }
+  */
+      /*
+  int n=5; 
+  for(int i=1; i<=n; i++){
+    for(int space=1; space<=i; space++){
+      cout<<" ";
+    }
+    for(int j=1; j<=n-i+1; j++){
+      cout<<"*";
+    }
+    for(int k=1; k<=n-i;k++){
       cout<<"*";
     }
     cout<<endl;
   }
+    */
+    /*
+  int n=5;
+  for(int i=1; i<=n; i++){
+    for(int space=1; space<=n-i+1; space++){
+      cout<<" ";
+    }
+    for(int j=1; j<=i; j++){
+      cout<<"*";
+    }
+    for(int j2=1; j2<=i-1; j2++){
+      cout<<"*";
+    }
+    cout<<endl; 
+  }
+    */
 
-
-  for(int rows3=1; rows3<=5; rows3++){
-    for(int columns3=1; columns3<=5-rows3+1;columns3++){
-      cout<<columns3;
+  /*
+  int n=5;
+  for(int i=1; i <= n; i++){
+    for(int j=1; j<=i; j++){
+     cout<<" ";
+    }
+    for(int k=1; k<=n-i+1; k++){
+      cout<<"*";
     }
     cout<<endl;
   }
- 
-  for(int rows=1; rows<=5; rows++){
-    //for every row, run the col
-  for(int columns=1; columns<=rows;columns++){
-    cout<<columns;
+    */
+/*
+  int n=5;
+  for(int i=1; i<=n; i++){
+  for(int j=1; j<=n-i+1; j++){
+    cout<<" ";
   }
-  cout<<endl;
- }
- return 0;
- }
+    for(int j=1; j<=i; j++){
+      cout<<"*";
+    }
+    cout<<endl;
+  }
+ */
+/*
+  int n = 5;
+  for (int i = 1; i <= 2 * n; i++) //row
+  {
+    int totslColsRow = i >= n ? 2 * n - i : i;
+    for (int j = 1; j <= totslColsRow; j++)//column
+    {
+      cout << "*";
+    }
+    cout << endl;
+  }
+*/
+  /*
+    for(int rows3=1; rows3<=5; rows3++){
+      for(int columns3=1; columns3<=5-rows3+1;columns3++){
+        cout<<columns3;
+      }
+      cout<<endl;
+    }
+
+    for(int rows=1; rows<=5; rows++){
+      //for every row, run the col
+    for(int columns=1; columns<=rows;columns++){
+      cout<<columns;
+    }
+    cout<<endl;
+   }
+    */
+  return 0;
+}
