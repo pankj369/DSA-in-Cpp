d2b247ac2d6d9ea5754ce4f8aa7be4df9e18bfe8
Pankaj Singh <psingh227003@gmail.com>
2025-07-31 16:44:39 +0530

    address of operator

---
+++ 16Pointers/1pointers.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+// !adress of operator (&-ampersand)
+/*int main(){ 
+  int a=10;
+  cout<<&a<<endl;
+  return 0;
+}  
+*/
+// !Pointers- special variables that store address of other variables
