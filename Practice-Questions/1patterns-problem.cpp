222115f90579f8619ff94712efb0d7e406a3e739
Pankaj Singh <psingh227003@gmail.com>
2025-08-02 19:12:35 +0530

    x shape

--- Practice-Questions/1patterns-problem.cpp
+++ Practice-Questions/1patterns-problem.cpp
@@ -65,7 +65,7 @@ int main()
 */
 
 // ! void triangle
-
+/*
 int main()
 {
   int n = 5;
@@ -88,6 +88,23 @@ int main()
       }
     }
     cout << endl;
+  }
+  return 0;
+}
+*/
+
+// ! X shape
+int main(){
+  int n=5;
+  for(int i=1; i<=n; i++){
+    for(int j=1; j<=n; j++){
+      if(j==i || j==n-i+1){
+        cout<<"*";
+      }else{
+        cout<<" ";
+      }
+    }
+    cout<<endl;
   }
   return 0;
 }
