3b312c74fc892d8d907b614e56e2628b6dfca8af
Pankaj Singh <psingh227003@gmail.com>
2025-08-04 00:06:43 +0530

    painters problem

--- 17Binary-search-algo/6Painters-partition.cpp
+++ 17Binary-search-algo/6Painters-partition.cpp
@@ -1,10 +1,42 @@
 #include <iostream>
 #include <vector>
+#include <climits>
 
 using namespace std;
 
+bool isPossible(vector<int> &arr, int n, int m, int maxAllowedTime){
+  int painters=1, time=0;
+  for(int i=0; i<n; i++){
+    if(time+arr[i]<= maxAllowedTime){
+      time+=arr[i];
+    }else{
+      painters++;
+      time=arr[i];
+    }
+  }
+  return painters<=n;
+}
+int minTimeToPaint(vector<int> &arr, int n, int m){
+  int sum=0;
+  int maxVal=INT_MIN;
+  for(int i=0; i<n; i++){
+    sum+=arr[i];
+    maxVal=max(maxVal, arr[i]);
+  }
+  int st=maxVal, end= sum, ans=-1;
+  while(st<=end){
+    int mid=st+(end-st)/2;
+    if(isPossible(arr, n,m,mid)){
+      ans=mid; end=mid-1;
+    }else{
+      st=mid+1;
+    }
+  }
+  return ans;
+}
 int main(){
-int a=20;
-cout<<&a;
+vector<int> arr={2,1,3,4};
+int n=4, m=2;
+cout<<minTimeToPaint(arr, n,m)<<endl;
 return 0;
 }
