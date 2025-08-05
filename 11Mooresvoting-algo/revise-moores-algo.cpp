ff8a1f360604b8c620f6fa849740a1aa1ed8d19c
Pankaj Singh <psingh227003@gmail.com>
2025-08-05 20:08:39 +0530

    practice-question

--- 11Mooresvoting-algo/revise-moores-algo.cpp
+++ 11Mooresvoting-algo/revise-moores-algo.cpp
@@ -3,8 +3,43 @@
 
 using namespace std;
 
-int main(){
-  int size=5;
-  vector<int< arr={}
+int majorityElem(vector<int> &arr)
+{
+  int count = 0, candidates = 0;
+  for (int num : arr)
+  {
+    if (count == 0)
+    {
+      candidates = num;
+    }
+    if (num == candidates)
+    {
+      count++;
+    }
+    else
+    {
+      count--;
+    }
+  }
+  // phase 2 : verify the valid candidates
+  count = 0;
+  for (int num : arr)
+  {
+    if (num == candidates)
+    {
+      count++;
+    }
+  }
+  if (count > arr.size() / 2)
+  {
+    return candidates;
+  }
+  return -1;
+}
+
+int main()
+{
+  vector<int> arr = {3, 3, 4, 5, 3, 8, 3, 3, 9};
+  cout << majorityElem(arr) << endl;
   return 0;
 }
