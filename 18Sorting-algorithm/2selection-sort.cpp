7715d5278586afdb5a9a1af8bc8a4fa7b00580f5
Pankaj Singh <psingh227003@gmail.com>
2025-08-06 07:50:46 +0530

    selection sort

---
+++ 18Sorting-algorithm/2selection-sort.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+// !Selection sort- is a kind of the opposite of bubble sort
+// time complexity = O(n^2)
+
+void selectionSort(int arr[], int n)
+{
+  for (int i = 0; i < n - 1; i++)
+  {
+    int smallestIndex = i;
+    for (int j = i + 1; j < n; j++)
+    {
+      if (arr[j] < arr[smallestIndex])
+      {
+        smallestIndex = j;
+      }
+    }
+    swap(arr[i], arr[smallestIndex]);
+  }
+}
+
+void printArray(int arr[], int n)
+{
+  for (int i = 0; i < n; i++)
+  {
+    cout << arr[i] << " ";
+  }
+  cout << endl;
+}
+
+int main()
+{
+  int n = 5;
+  int arr[] = {4, 1, 5, 2, 3};
+
+  selectionSort(arr, n);
+  printArray(arr, n);
+  return 0;
+}
