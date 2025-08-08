5f99dcc37ecf2e034f00c6804eece8361a5cba13
Pankaj Singh <psingh227003@gmail.com>
2025-08-08 16:25:29 +0530

    dutch national flag algo

--- 18Sorting-algorithm/4Sorting-0s-1s-2s.cpp
+++ 18Sorting-algorithm/4Sorting-0s-1s-2s.cpp
@@ -1,7 +1,10 @@
 #include <iostream>
 #include <vector>
-using namespace std;
+#include <algorithm>
 
+using namespace std;
+/*
+// !Brute force
 void sortArray(vector<int>& nums){
   int n =  nums.size();
   int count0=0, count1=0, count2=0;
@@ -34,3 +37,35 @@ int main(){
   cout<<endl;
   return 0;
 }
+  */
+
+  // !Dutch National flag algorithm - we can sort the numbers in a particular order with O(n) time complexity with single pass
+  
+  void sortColors(vector<int>& nums){
+    int n= nums.size();
+    int low=0, mid = 0, high=n-1;
+
+    while(mid<=high){
+      if(nums[mid]==0){
+        swap(nums[low], nums[mid]);
+        mid++;
+        low++;
+      }else if(nums[mid] ==1){
+        mid++;
+      }else{
+        swap(nums[high], nums[mid]);
+        high--;
+      }
+    }
+  }
+
+  int main(){
+    vector<int> nums={2,1,0,2,0,1};
+    sortColors(nums);
+
+    for(int i=0; i<nums.size(); i++){
+      cout<<nums[i]<<" ";
+    }
+    cout<<endl;
+    return 0;
+  }
