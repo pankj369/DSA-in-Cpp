a409e7a5045725b726a8122c670f3dac5727a7f3
Pankaj Singh <psingh227003@gmail.com>
2025-08-21 17:04:13 +0530

    removing all occurance number

--- 19String/2ValidPalindrome.cpp
+++ 19String/2ValidPalindrome.cpp
@@ -6,12 +6,13 @@
 using namespace std;
 
 // ! Is Valid palindrome or not
-
+/*
 bool isPalindrome(string s) {
     int st = 0, end = s.length() - 1;
 
     while (st < end) {
         // Skip non-alphanumeric characters
+        //checking alpha numberic  
         if (!isalnum(s[st])) {
             st++;
             continue;
@@ -42,4 +43,23 @@ int main() {
     }
 
     return 0;
+}
+*/
+
+// !Remove all Occurrences
+
+// Find to remove all accurrences of a substring 'part' from string 's'
+string removeOccurance(string s, string part){
+    while(s.length()> 0 && s.find(part)<s.length()){
+        s.erase(s.find(part), part.length());
+    }
+    return s;
+}
+int main(){
+
+   string s = "daabcbaabcbc";
+   string part = "abc";
+   string result = removeOccurance(s, part);
+       cout << "Resultant string after removing all occurrences of '" << part << "': " << result << endl;
+       return 0;
 }
