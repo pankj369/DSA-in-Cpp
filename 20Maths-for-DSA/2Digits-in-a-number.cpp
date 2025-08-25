98b6ef20788e10259300d05a06c2f5f2fe6590b4
Pankaj Singh <psingh227003@gmail.com>
2025-08-25 09:41:14 +0530

    Digits-in-a-number

--- 20Maths-for-DSA/2Digits-in-a-number.cpp
+++ 20Maths-for-DSA/2Digits-in-a-number.cpp
@@ -1,25 +1,29 @@
 #include <iostream>
+#include <cmath>
+
 using namespace std;
 
-void printDigits(int n){
+void printDigits(int n)
+{
   // int count=0;/
-  int sum=0;
-  while(n!=0){
-    int digit= n%10;
+  int sum = 0;
+  while (n != 0)
+  { // T.C= O(log base 10 n)
+    int digit = n % 10;
     // cout<<digit<<endl;
     // count++; //to print total number of digits
-    sum +=digit; //to print sum of all digits in a number
-    n=n/10;
+    sum += digit; // to print sum of all digits in a number
+    n = n / 10;
   }
   // cout<<count<<endl;
-  cout<<sum<<endl;
+  cout << sum << endl;
 }
 
+int main()
+{
+  int n = 3568;
 
-int main(){
-int n=3568;
-
-printDigits(n);
-return 0;
-
+  // printDigits(n);
+  cout << (int)(log10(n) + 1) << endl;
+  return 0;
 }
