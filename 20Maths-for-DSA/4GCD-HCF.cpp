52cc86ae1ac53c126608ef92d14e636ad26ef228
Pankaj Singh <psingh227003@gmail.com>
2025-08-25 10:29:53 +0530

    GCD or HCF with Euclids Algo

---
+++ 20Maths-for-DSA/4GCD-HCF.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+//!GCD or HCF with Euclids algorithm
+int gcd(int a, int b){
+  while(a>0 && b>0){
+    if(a>b){
+      a=a%b;
+    }else{
+      b=b%a;
+    }
+  }
+  if(a==0) return b;
+  return a;
+}
+
+int main(){
+  cout<<gcd(20,28)<<endl;
+  return 0;
+}
