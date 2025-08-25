0dda6dd1ec497a339d35ab09bd2aada66f406b4c
Pankaj Singh <psingh227003@gmail.com>
2025-08-25 10:41:59 +0530

    lcm-with-Euclid-algo

--- 20Maths-for-DSA/4GCD-HCF.cpp
+++ 20Maths-for-DSA/4GCD-HCF.cpp
@@ -2,7 +2,8 @@
 #include <cmath>
 
 using namespace std;
-//!GCD or HCF with Euclids algorithm
+//! GCD or HCF with Euclids algorithm
+/*
 int gcd(int a, int b){
   while(a>0 && b>0){
     if(a>b){
@@ -17,5 +18,29 @@ int gcd(int a, int b){
 
 int main(){
   cout<<gcd(20,28)<<endl;
+  return 0;
+}
+  */
+
+// !LCM with Euclids algo
+
+int gcd(int a, int b)
+{
+  while (b != 0)
+  {
+    int temp = b;
+    b = a % b;
+    a = temp;
+  }
+  return a;
+}
+int lcm(int a, int b)
+{
+  int g = gcd(a, b);
+  return (a * b) / g;
+}
+int main()
+{
+  cout << lcm(20, 28) << endl;
   return 0;
 }
