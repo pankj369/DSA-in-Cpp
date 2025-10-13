49a1ca635ac494a7a271282eb6d3f1f0764d753c
pankj369 <psingh227003@gmail.com>
2025-10-13 14:34:38 +0530

    practice questions

--- Practice-Questions/array.cpp
+++ Practice-Questions/array.cpp
@@ -66,7 +66,7 @@ int main()
 */
 
 // !reverse an array
-
+/*
 void reversedArray(int arr[], int size)
 {
     int st = 0, end = size - 1;
@@ -87,5 +87,123 @@ int main()
     {
         cout << arr[i]<<" ";
     }
+    return 0;
+}
+
+*/
+
+// !WAF to calculate sum and product of all numbers in an array
+/*
+
+int main()
+{
+    int arr[] = {4, 5, 6};
+    int size = 3;
+    int sum = 0;
+    int product = 1;
+    for (int i = 0; i <= size - 1; i++)
+    {
+        sum += arr[i];
+        product *= arr[i];
+    }
+    cout << "Sum : " << sum << endl;
+    cout << "Product : " << product << endl;
+    return 0;
+}
+*/
+// * again using seperate function
+/*
+void sumAndProdcut(int arr[], int size){
+    int sum=0;
+    int product=1;
+
+    for(int i=0; i<size; i++){
+        sum+=arr[i];
+        product*=arr[i];
+    }
+    cout<<"Sum of an arrya Elements : "<<sum<<endl;
+    cout<<"Product of an array Elements : "<<product<<endl;
+}
+int main(){
+    int arr[]={4,5,6};
+    int size=3;
+    sumAndProdcut(arr,size);
+    return 0;
+}
+    */
+// !WAF to swap the max & min number of an array
+#include <climits> //for large bit integers size greater that 8 bits atleast
+                   /*
+                   void swapMaxMin(int arr[], int size)
+                   {
+                       int maxIndex = 0, minIndex = 0;
+                       for (int i = 0; i < size; i++)
+                       {
+                           if (arr[i] > arr[maxIndex])
+                               maxIndex = i;
+                           if (arr[i] < arr[minIndex])
+                               minIndex = i;
+                       }
+                       swap(arr[maxIndex], arr[minIndex]);
+                       cout << "Array after swapping max and min: ";
+                       for (int i = 0; i < size; i++)
+                           cout << arr[i] << " ";
+                   }
+                   int main()
+                   {
+                       int arr[] = {45, 7989, 12, -549, 6478, -46414921};
+                       int size = 6;
+                       swapMaxMin(arr, size);
+                       return 0;
+                   }
+                   */
+
+// !WAF to print all the unique values in an array
+/*
+void printUnique(int arr[], int size)
+{
+    cout<<"Unique Elements : ";
+    for (int i = 0; i < size; i++)
+    {
+        int count = 0;
+        for (int j = 0; j < size; j++)
+        {
+            if (arr[i] == arr[j])
+                count++;
+        }
+        if (count == 1)
+            cout << arr[i] << " ";
+    }
+}
+int main()
+{
+    int arr[] = {1, 2, 3, 1, 2, 4, 3, 1, 4, 8};
+    int size = 10;
+    printUnique(arr, size);
+    return 0;
+}
+*/
+
+// !WAF to print intersection of 2 arrays
+void intersection(int arr1[], int size1, int arr2[], int size2)
+{
+    cout << "Intersection : ";
+    for (int i = 0; i < size1; i++)
+    {
+        for (int j = 0; j < size2; j++)
+        {
+            if (arr1[i] == arr2[j]){
+                cout << arr1[i] << " ";
+            }
+        }
+    }
+}
+int main()
+{
+    int arr1[] = {1, 2, 3, 4, 5, 6};
+    int arr2[] = {3, 4, 5, 6, 7};
+    int size1 = 6;
+    int size2 = 5;
+    intersection(arr1, size1, arr2, size2);
     return 0;
 }
