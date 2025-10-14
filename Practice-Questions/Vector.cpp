caf5b51c47ff617d34ce01c452ed87aa12d8760c
pankj369 <psingh227003@gmail.com>
2025-10-14 15:50:00 +0530

    practice linearsearch, uniqueNum, reverse using vector

--- Practice-Questions/Vector.cpp
+++ Practice-Questions/Vector.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <vector> //
 using namespace std;
-
+/*
 int main()
 {
     // vector<int> vec;             // initialise 1
@@ -21,6 +21,75 @@ int main()
     // vec.front();
     cout<<vec.front()<<endl;
     cout<<vec.back()<<endl;
-    
+
+    return 0;
+}
+*/
+
+//! Reverse an array using two pointers approch and also use vector
+/*
+void reversedArray(vector <int> &arr){
+    int st=0;
+    int end=arr.size()-1;
+    while(st<end){
+        swap(arr[st],arr[end]);
+        st++;
+        end--;
+    }
+}
+
+int main(){
+    vector<int> arr={6,5,4,3,2,1};
+
+    cout<<"Original array: ";
+    for(int i : arr){
+        cout<<i<<" ";
+    }
+    reversedArray(arr);
+
+    cout<<"\nReversed array: ";
+    for(int i : arr){
+        cout<<i<<" ";
+    }
+    cout<<endl;
+    return 0;
+}
+*/
+
+//! finding unique or single number
+/*
+int uniqueNum(vector<int>&arr){
+    int ans=0;
+    for(int num : arr){
+      ans^=num;
+    }
+    return ans;
+}
+int main(){
+    vector<int>arr={1,4,2,5,4,2,1};
+    int size=5;
+    cout<<uniqueNum(arr);
+    return 0;
+}
+*/
+
+//! linear search
+int linearSearch(vector<int> &arr, int target)
+{
+    for (int i = 0; i < arr.size(); i++)
+    {
+        if (arr[i] == target)
+        {
+            return i;
+        }
+    }
+    return -1;
+}
+int main()
+{
+    vector<int> arr = {7, 5, 6, 8, 3, 2, 48, 54, 6};
+    int size = 9;
+    int target = 8;
+    cout << "Target found at index : " << linearSearch(arr, target);
     return 0;
 }
