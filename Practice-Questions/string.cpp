9818f78f67395072059c7e80217eabbea0f8a75e
pankj369 <psingh227003@gmail.com>
2025-10-16 17:53:48 +0530

    string

---
+++ Practice-Questions/string.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+
+int main()
+{
+    char str[] = "Pankaj Kumar Singh";
+    cout << "Str : " << str << endl;
+
+    char name[20] = "Pankaj Kumar Singh ";
+    cout << "using array: " << name << endl;
+
+    // input method
+    char name1[20];
+    cin >> name1; //this will read input until whitespace
+    cout << "Using cin : " << name1 << endl;
+
+    // uisng cin.getline() function
+    cin.getline(name1, 100); //this will read full line including whitespace
+    cout << "using cin.getline : " << name1 << endl;
+
+
+
+    string s1 = "Hello";
+    string s2 = "Pankj";
+    string s3 = s1 + " " + s2;
+    cout << s3 << endl;
+    return 0;
+}
