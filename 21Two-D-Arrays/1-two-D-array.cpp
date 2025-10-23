9044604c6d3017e96e2fc101af60906c0f99dfb1
pankj369 <psingh227003@gmail.com>
2025-10-23 12:24:19 +0530

    maximum row sum

--- 21Two-D-Arrays/1-two-D-array.cpp
+++ 21Two-D-Arrays/1-two-D-array.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
-using namespace std;
+#include <climits>
 
+using namespace std;
+// ? linear search in 2d array
 pair<int, int> searchMatrix(int matrix[][3], int rows, int cols, int key) {
     for (int i = 0; i < rows; i++) {
         for (int j = 0; j < cols; j++) {
@@ -12,21 +14,37 @@ pair<int, int> searchMatrix(int matrix[][3], int rows, int cols, int key) {
     return {-1, -1}; // if not found
 }
 
+
+// ? maximum row sum
+int getMaxSum(int matrix[][3], int rows, int cols){
+    int maxRowSum=INT8_MIN;
+    for(int i=0; i<rows; i++){
+        int rowSum=0;
+        for(int j=0; j<cols; j++){
+            rowSum+=matrix[i][j];
+        }    
+        maxRowSum=max(maxRowSum, rowSum);
+    }
+    return maxRowSum;
+}
+
 int main() {
-    int matrix[4][3] = {
+    int matrix[3][3] = {
         {1, 2, 3},
         {4, 5, 6},
-        {7, 8, 9},
-        {10, 11, 12}
+        {7, 8, 9}
     };
 
-    int rows = 4, cols = 3, key = 9;
-    pair<int, int> pos = searchMatrix(matrix, rows, cols, key);
+    int rows = 3, cols = 3, key = 9;
 
-    if (pos.first != -1)
-        cout << "Key found at position: (" << pos.first << ", " << pos.second << ")" << endl;
-    else
-        cout << "Key not found!" << endl;
+    cout<<getMaxSum(matrix, rows, cols)<<endl;
+
+    // pair<int, int> pos = searchMatrix(matrix, rows, cols, key);
+
+    // if (pos.first != -1)
+    //     cout << "Key found at position: (" << pos.first << ", " << pos.second << ")" << endl;
+    // else
+    //     cout << "Key not found!" << endl;
 
     return 0;
 }
