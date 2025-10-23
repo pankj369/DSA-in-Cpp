8338c6d5bf1faca5c703871ef303a2897f77042c
pankj369 <psingh227003@gmail.com>
2025-10-23 11:41:36 +0530

    2D array

--- 21Two-D-Arrays/1-two-D-array.cpp
+++ 21Two-D-Arrays/1-two-D-array.cpp
@@ -1,8 +1,33 @@
 #include <iostream>
 using namespace std;
 
-int main(){
-  cout<<"Two-D-Array"<<endl;
-
-  return 0;
+bool searchMatrix(int matrix[][3], int rows, int cols, int key) {
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            if (matrix[i][j] == key) {
+                return true; // key found
+            }
+        }
+    }
+    return false; // key not found
 }
+
+int main() {
+    int matrix[4][3] = {
+        {1, 2, 3},
+        {4, 5, 6},
+        {7, 8, 9},
+        {10, 11, 12}
+    };
+
+    int rows = 4;
+    int cols = 3;
+    int key = 8;
+
+    if (searchMatrix(matrix, rows, cols, key))
+        cout << "Key found!" << endl;
+    else
+        cout << "Key not found!" << endl;
+
+    return 0;
+  }
